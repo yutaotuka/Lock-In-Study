a91a36648b3334f5f909d42913a1cad2c52df51c
yutaotuka <bmjl1125@gmail.com>
2024-03-25 01:38:08 +0900

    line_login 実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :answers
   has_many :study_records
+
+  validates :line_user_id, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_124718) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_24_154532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,10 +35,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_124718) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "line_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "line_user_id", null: false
+    t.string "name", null: false
+    t.index ["line_user_id"], name: "index_users_on_line_user_id", unique: true
   end
 
   add_foreign_key "answers", "users"
