37a897de902b08a0ba07b9b6115085172027fb65
yutaotuka <bmjl1125@gmail.com>
2024-03-25 23:03:46 +0900

    //= require rails-ujsを追記

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import Rails from "@rails/ujs"
-// Rails.start()
+//= require rails-ujs
 import "@hotwired/turbo-rails"
 import "controllers"
