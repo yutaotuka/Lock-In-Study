c5ec43996afa3fdd35b19ac6b990c69eb5842d51
yutaotuka <bmjl1125@gmail.com>
2024-03-28 22:52:18 +0900

    study_records_controller no のcurrent_user.idに書き換え

--- app/controllers/study_records_controller.rb
+++ app/controllers/study_records_controller.rb
@@ -1,6 +1,6 @@
 class StudyRecordsController < ApplicationController
   def start
-    @study_record = StudyRecord.new(start_time: Time.current, user_id: current.user_id)
+    @study_record = StudyRecord.new(start_time: Time.current, user_id: current_user.id)
     if @study_record.save
       render json: { success: true, study_record_id: @study_record_id }
     else
