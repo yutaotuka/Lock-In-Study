9e93ef38e3cc28300118dbd675fce25b9ce3087b
yutaotuka <bmjl1125@gmail.com>
2024-05-02 21:31:24 +0900

    test

--- app/controllers/study_time_controller.rb
+++ app/controllers/study_time_controller.rb
@@ -5,6 +5,6 @@ class StudyTimeController < ApplicationController
 
     # 週ごとの合計、最新の週のみ
     weekly_totals = StudyRecord.group_by_week(:created_at, week_start: :monday).sum(:duration_time)
-    @weekly_totals = weekly_totals.to_a.last(7).to_h
+    @weekly_totals = weekly_totals.to_a.last(10).to_h
   end
 end
