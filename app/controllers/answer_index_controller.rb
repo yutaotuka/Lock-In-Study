faf2aadef4966437e759ca4595a8d2582fa0ffc6
yutaotuka <bmjl1125@gmail.com>
2024-05-15 11:37:54 +0900

    answerのdailyの表示

--- app/controllers/answer_index_controller.rb
+++ app/controllers/answer_index_controller.rb
@@ -1,5 +1,10 @@
 class AnswerIndexController < ApplicationController
   def index
-    @answers = Answer.all
+    # 今日を含む過去10日間の範囲を指定
+    start_date = 10.days.ago.beginning_of_day
+    end_date = Time.zone.now.end_of_day
+  
+    # その期間内のAnswerレコードを取得
+    @answers = Answer.where(created_at: start_date..end_date).order('created_at DESC')
   end
 end
