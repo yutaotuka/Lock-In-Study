28eb99871771b52ef8a360e084cf9ac4936a797f
yutaotuka <bmjl1125@gmail.com>
2024-05-17 11:21:16 +0900

    answer_indexのweekly表示

--- app/controllers/answer_index_controller.rb
+++ app/controllers/answer_index_controller.rb
@@ -1,10 +1,13 @@
 class AnswerIndexController < ApplicationController
   def index
     # 今日を含む過去10日間の範囲を指定
-    start_date = 10.days.ago.beginning_of_day
+    start_date_for_day = 10.days.ago.beginning_of_day
     end_date = Time.zone.now.end_of_day
+    # 1ヶ月
+    start_date_for_week = 4.weeks.ago.beginning_of_day
   
     # その期間内のAnswerレコードを取得
-    @answers = Answer.where(created_at: start_date..end_date).order('created_at DESC')
+    @answers = Answer.where(created_at: start_date_for_day..end_date).order('created_at DESC')
+    @answers_weekly = Answer.where(created_at: start_date_for_week..end_date).order('created_at DESC')
   end
 end
