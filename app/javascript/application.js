89536e04642d109a276536595e48dd430a651c19
yutaotuka <bmjl1125@gmail.com>
2024-05-27 19:07:31 +0900

    重複しているJSファイルの内容を消した

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -114,22 +114,3 @@ document.addEventListener('form-completed', function() {
   }
 });
 
-// study_time_index
-//任意のタブにURLからリンクするための設定
-function GethashID (hashIDName){
-  if(hashIDName){
-    //タブ設定
-    $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-      var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得 
-      if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-        var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-        $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
-        $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
-        //表示させるエリア設定
-        $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
-        $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加 
-      }
-    });
-  }
-}
-
