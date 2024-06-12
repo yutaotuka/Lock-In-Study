69972723909c59652ebc3b7c4a0901f184d5b7ef
yutaotuka <bmjl1125@gmail.com>
2024-06-12 11:03:23 +0900

    勉強時間（日）のデータがcurrent_userのもののみにした

--- app/controllers/study_time_controller.rb
+++ app/controllers/study_time_controller.rb
@@ -1,6 +1,6 @@
 class StudyTimeController < ApplicationController
   def index
-    totals = StudyRecord.group_by_day(:created_at).sum(:duration_time)
+    totals = current_user.study_records.group_by_day(:created_at).sum(:duration_time)
     @daily_totals = totals.to_a.last(10).to_h
 
     # 週ごとの合計、最新の週のみ
