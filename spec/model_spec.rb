8d01e20749bd174d20026d136c6aac4418a0b916
yutaotuka <bmjl1125@gmail.com>
2024-06-16 19:01:23 +0900

    Fix: エラーメッセージ修正

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe User, type: :model do
   it "line_user_idがない場合は無効" do
     user = User.new(line_user_id: "", name: "無効 太郎")
     expect(user).to_not be_valid
-    expect(user.errors[:line_user_id]).to include("入力してくださ")
+    expect(user.errors[:line_user_id]).to include("を入力してください")
   end
 
   it "重複したline_user_idは無効" do
     User.create(line_user_id: "unique_id1234", name: "重複  太郎")
     user = User.new(line_user_id: "unique_id1234", name: "重複  二郎")
     expect(user).to_not be_valid
-    expect(user.errors[:line_user_id]).to include("すでに存在します")
+    expect(user.errors[:line_user_id]).to include("はすでに存在します")
   end
 
   it "名前がない場合は無効" do
     user = User.new(line_user_id: "id1234567890", name: "")
     expect(user).to_not be_valid
-    expect(user.errors[:name]).to include("入力してください")
+    expect(user.errors[:name]).to include("を入力してください")
   end
 end
