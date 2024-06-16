9c2b99aca7aeef3a74b0b2cd7853eb7d3cd2e889
yutaotuka <bmjl1125@gmail.com>
2024-06-16 18:58:49 +0900

    Fix:エラーメッセージが日本語だったので修正

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe User, type: :model do
   it "line_user_idがない場合は無効" do
     user = User.new(line_user_id: "", name: "無効 太郎")
     expect(user).to_not be_valid
-    expect(user.errors[:line_user_id]).to include("can't be blank")
+    expect(user.errors[:line_user_id]).to include("入力してくださ")
   end
 
   it "重複したline_user_idは無効" do
     User.create(line_user_id: "unique_id1234", name: "重複  太郎")
     user = User.new(line_user_id: "unique_id1234", name: "重複  二郎")
     expect(user).to_not be_valid
-    expect(user.errors[:line_user_id]).to include("has already been taken")
+    expect(user.errors[:line_user_id]).to include("すでに存在します")
   end
 
   it "名前がない場合は無効" do
     user = User.new(line_user_id: "id1234567890", name: "")
     expect(user).to_not be_valid
-    expect(user.errors[:name]).to include("can't be blank")
+    expect(user.errors[:name]).to include("入力してください")
   end
 end
