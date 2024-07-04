6a2a17c7e3135b75f11614df6c648d1a2e81dbaf
yutaotuka <bmjl1125@gmail.com>
2024-07-04 19:43:15 +0900

    修正

--- spec/system/line_login_spec.rb
+++ spec/system/line_login_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe 'LINE Login', type: :system do
   end
 
   it 'LINEログインが成功する' do
-    visit line_login_api_login_path
+    visit line_login_api_login_path(redirect_uri: ' 
+    https://www.lockinstudy.com/line_login_api/callback')
     expect(page).to have_content('ログインしました')
   end
 end
