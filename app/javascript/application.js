e334213f3b94dac9fce1938cea9b5fd28c2a1ab2
yutaotuka <bmjl1125@gmail.com>
2024-08-20 01:13:47 +0900

    質問通知時間が短かったので修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -97,7 +97,7 @@ document.addEventListener('turbo:load', function() {
             }
           });
       }
-    }, 120000); // 1分＝60000
+    }, 1200000); // 1分＝60000
   });
 
   stopBtn.addEventListener('click', function() {
